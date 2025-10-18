8091bf181e214f1f55297318681a3b72d101b43a
proxyparty <alexisc9697@gmail.com>
2025-10-17 23:05:10 -0400

    Completed the basic layout for the 'Our Work' page with placeholder images.

--- src/app/our-work/page.js
+++ src/app/our-work/page.js
@@ -8,9 +8,101 @@ export default function OurWork() {
       <div className={styles.outerContainer}>
         <div className={styles.innerContainerA}>
           <div className={styles.slimHero}>
-            <p>Our Work</p>
+            <h1>Our Work</h1>
             <div className={styles.tint}></div>
             <Image src="/our_work_image.png" alt="logo" layout="fill" />
+          </div>
+        </div>
+        <div className={styles.innerContainerB}>
+          <h2 className={styles.sectionTitle}>Before & After</h2>
+          <div className={styles.threeCol}>
+            <div className={`${styles.imageContainer} ${styles.before}`}>
+              <Image
+                src="/personal_services_image.png"
+                alt="logo"
+                layout="fill"
+              />
+            </div>
+            <div className={styles.arrowIconContainer}>
+              <Image src="/green_arrow_right.png" alt="logo" layout="fill" />
+            </div>
+            <div className={`${styles.imageContainer} ${styles.after}`}>
+              <Image
+                src="/personal_services_image.png"
+                alt="logo"
+                layout="fill"
+              />
+            </div>
+          </div>
+          <div className={styles.threeCol}>
+            <div className={`${styles.imageContainer} ${styles.before}`}>
+              <Image
+                src="/personal_services_image.png"
+                alt="logo"
+                layout="fill"
+              />
+            </div>
+            <div className={styles.arrowIconContainer}>
+              <Image src="/green_arrow_right.png" alt="logo" layout="fill" />
+            </div>
+            <div className={`${styles.imageContainer} ${styles.after}`}>
+              <Image
+                src="/personal_services_image.png"
+                alt="logo"
+                layout="fill"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.innerContainerC}>
+          <h2 className={styles.sectionTitle}>Gallery</h2>
+          <div className={styles.galleryContainer}>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.galleryImage}>
+              <Image src="/land_clearing_image.png" alt="logo" layout="fill" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.innerContainerD}>
+          <div className={styles.twoColumn}>
+            <div className={styles.columnImage}>
+              <Image src="/estimate_image.png" alt="logo" layout="fill" />
+            </div>
+            <div className={styles.columnText}>
+              <h1>Get Your Estimate Today</h1>
+              <p>
+                Whether you’re planning a pool demolition, backyard
+                transformation, foundation excavation, or land clearing project,
+                Rocks Masonry and Excavation is your trusted local expert.
+              </p>
+              <p>
+                Call us today for a <b>FREE</b> estimate:
+              </p>
+              <div className={styles.ctaBtn}>Contact Us</div>
+            </div>
           </div>
         </div>
       </div>
